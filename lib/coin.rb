49d1f33735f259fdffe052ef064c78c21f95f464
Sowmya Dinavahi and Sean Pierce <sowmya@>
2017-04-20 14:00:56 -0700

    refactor hash

--- lib/coin.rb
+++ lib/coin.rb
@@ -2,29 +2,29 @@ class Fixnum
   define_method :change do
   cents = self
   returned_change = {
-    'quarters' => [[30, 0]],
-    'dimes' => [[30, 0]],
-    'nickels' => [[30, 0]],
-    'pennies' => [[30, 0]]
+    'quarters' => [30, 0],
+    'dimes' => [30, 0],
+    'nickels' => [30, 0],
+    'pennies' => [30, 0]
   }
 
   until cents < 25 do
-    returned_change['quarters'][0][1] = returned_change['quarters'][0][1] + 1
-    returned_change['quarters'][0][0] = returned_change['quarters'][0][0] - 1
+    returned_change['quarters'][1] = returned_change['quarters'][1] + 1
+    returned_change['quarters'][0] = returned_change['quarters'][0] - 1
     cents = cents - 25
   end #end quarters loop
   until cents < 10 do
-    returned_change['dimes'][0][1] = returned_change['dimes'][0][1] + 1
-    returned_change['dimes'][0][0] = returned_change['dimes'][0][0] - 1
+    returned_change['dimes'][1] = returned_change['dimes'][1] + 1
+    returned_change['dimes'][0] = returned_change['dimes'][0] - 1
     cents = cents - 10
   end
   until cents < 5 do
-    returned_change['nickels'][0][1] = returned_change['nickels'][0][1] + 1
-    returned_change['nickels'][0][0] = returned_change['nickels'][0][0] - 1
+    returned_change['nickels'][1] = returned_change['nickels'][1] + 1
+    returned_change['nickels'][0] = returned_change['nickels'][0] - 1
     cents = cents - 5
   end
-  returned_change['pennies'][0][1] = returned_change['pennies'][0][1] + cents
-  returned_change['pennies'][0][0] = returned_change['pennies'][0][0] - cents
+  returned_change['pennies'][1] = returned_change['pennies'][1] + cents
+  returned_change['pennies'][0] = returned_change['pennies'][0] - cents
   returned_change
   end
 end
