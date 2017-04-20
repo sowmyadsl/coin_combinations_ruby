d55255df2a481446b2241a10b4f0cf373db30caa
Sowmya Dinavahi and Sean Pierce <sean@>
2017-04-20 09:48:23 -0700

    complete logic to separate change as appropriate values

--- lib/coin.rb
+++ lib/coin.rb
@@ -1,23 +1,33 @@
 class Fixnum
   define_method :change do
   cents = self
-  counter = cents
   returned_change = {
     'quarters' => [],
     'dimes' => [],
     'nickels' => [],
     'pennies' => []
   }
-    until counter.==(0)
-      if cents >= 25
-        returned_change['quarters'].push(1)
-        cents = cents - 25
-        counter = counter - 25
-      else
-        counter = counter - 1
-      end # ends if else statement
-    end #ends until loop
-    returned_change['pennies'].push(cents)
-    returned_change
-  end #ends change method
+  num_of_quarters = 0
+  num_of_dimes = 0
+  num_of_nickels = 0
+  num_of_pennies = 0
+
+  until cents < 25 do
+    num_of_quarters = num_of_quarters + 1
+    cents = cents - 25
+  end #end quarters loop
+  until cents < 10 do
+    num_of_dimes = num_of_dimes + 1
+    cents = cents - 10
+  end
+  until cents < 5 do
+    num_of_nickels = num_of_nickels + 1
+    cents = cents - 5
+  end
+  returned_change['quarters'].push(num_of_quarters)
+  returned_change['dimes'].push(num_of_dimes)
+  returned_change['pennies'].push(cents)
+  returned_change['nickels'].push(num_of_nickels)
+  returned_change
+  end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -2,10 +2,16 @@ require ('coin')
 require ('rspec')
 
 describe("Fixnum#change") do
-  it("will receive a number as input and return the same number") do
-    expect(43.change).to(eq(43))
+  # it("will receive a number as input and return the same number") do
+  #   expect(43.change).to(eq(43))
+  # end
+  # it("will receive a number from the user as input and return the greatest number of quarters that could possibly exist within that number") do
+  #   expect(43.change).to(eq({"quarters"=>[1], "dimes"=>[], "nickels"=>[], "pennies"=>[18]}))
+  # end
+
+  it("will receive a number from the user as input and return the greatest number of quarters, then the greatest number of dimes, etc (for nuckels and pennies, too) that could possibly exist within that number") do
+    expect(43.change).to(eq({"quarters"=>[1], "dimes"=>[1], "nickels"=>[1], "pennies"=>[3]}))
   end
-  it("  will receive a number from the user as input and return the greatest number of quarters that could possibly exist within that number") do
-    expect(43.change).to(eq("one quarter"))
-  end
+
+
 end
