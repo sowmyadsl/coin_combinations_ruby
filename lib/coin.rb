3bf06a152e8558971b3fdf9a297133ede62820d2
Sowmya Dinavahi and Sean Pierce <sean@>
2017-04-20 10:44:56 -0700

    refactor logic to store the initial number of coins and the returned number of coins in a hash

--- lib/coin.rb
+++ lib/coin.rb
@@ -2,32 +2,34 @@ class Fixnum
   define_method :change do
   cents = self
   returned_change = {
-    'quarters' => [],
-    'dimes' => [],
-    'nickels' => [],
-    'pennies' => []
+    'quarters' => [[30, 0]],
+    'dimes' => [[30, 0]],
+    'nickels' => [[30, 0]],
+    'pennies' => [[30, 0]]
   }
-  num_of_quarters = 0
-  num_of_dimes = 0
-  num_of_nickels = 0
-  num_of_pennies = 0
 
   until cents < 25 do
-    num_of_quarters = num_of_quarters + 1
+    returned_change['quarters'][0][1] = returned_change['quarters'][0][1] + 1
+    returned_change['quarters'][0][0] = returned_change['quarters'][0][0] - 1
     cents = cents - 25
   end #end quarters loop
   until cents < 10 do
-    num_of_dimes = num_of_dimes + 1
+    returned_change['dimes'][0][1] = returned_change['dimes'][0][1] + 1
+    returned_change['dimes'][0][0] = returned_change['dimes'][0][0] - 1
     cents = cents - 10
   end
   until cents < 5 do
-    num_of_nickels = num_of_nickels + 1
+    returned_change['nickels'][0][1] = returned_change['nickels'][0][1] + 1
+    returned_change['nickels'][0][0] = returned_change['nickels'][0][0] - 1
     cents = cents - 5
   end
-  returned_change['quarters'].push(num_of_quarters)
-  returned_change['dimes'].push(num_of_dimes)
-  returned_change['pennies'].push(cents)
-  returned_change['nickels'].push(num_of_nickels)
+  returned_change['pennies'][0][1] = returned_change['pennies'][0][1] + cents
+  returned_change['pennies'][0][0] = returned_change['pennies'][0][0] - cents
+
+  # returned_change['quarters'].push(num_of_quarters)
+  # returned_change['dimes'].push(num_of_dimes)
+  # returned_change['nickels'].push(num_of_nickels)
+  # returned_change['pennies'].push(cents)
   returned_change
   end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -10,7 +10,10 @@ describe("Fixnum#change") do
   # end
 
   it("will receive a number from the user as input and return the greatest number of quarters, then the greatest number of dimes, etc (for nuckels and pennies, too) that could possibly exist within that number") do
-    expect(43.change).to(eq({"quarters"=>[1], "dimes"=>[1], "nickels"=>[1], "pennies"=>[3]}))
+    expect(43.change).to(eq({"quarters"=>[[29, 1]], "dimes"=>[[29, 1]], "nickels"=>[[29, 1]], "pennies"=>[[27, 3]]}))
+  end
+  it("will start with a pre dertermines amount of coins in the stash, and will subtract each coin given as change") do
+    expect(73.change).to(eq({"quarters"=>[[28, 2]], "dimes"=>[[28, 2]], "nickels"=>[[30, 0]], "pennies"=>[[27, 3]]}))
   end
 
 
